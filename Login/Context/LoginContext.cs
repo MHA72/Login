080bf935806d61f542485399dedca5936a9c906e
MohammadHossein <hossinarab727@gmail.com>
2023-11-02 13:08:54 +0330

    Implement Config and introduce Model

--- Login/Context/LoginContext.cs
+++ Login/Context/LoginContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Login.Configurations.User;
+using Login.Models.User;
+using Microsoft.EntityFrameworkCore;
 
 namespace Login.Context;
 
@@ -6,9 +8,13 @@ public class LoginContext : DbContext
 {
     public LoginContext(DbContextOptions<LoginContext> options) : base(options){}
 
+    public DbSet<User>? Users { get; set; }
+    public DbSet<UserLogin>? UserLogins { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfiguration(new UserConfiguration());
+        modelBuilder.ApplyConfiguration(new UserLoginConfiguration());
     }
 }
