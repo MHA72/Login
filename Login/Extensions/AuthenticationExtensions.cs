384e0b40ab2eec1fcbd81ac9a4f2f92c938c4c96
MohammadHossein <hossinarab727@gmail.com>
2023-11-02 14:08:46 +0330

    Add Service JWT

---
+++ Login/Extensions/AuthenticationExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Login.Extensions;
+
+public static class AuthenticationExtensions
+{
+    public static IServiceCollection AddAuth(this IServiceCollection serviceCollection, IConfiguration configuration)
+    {
+        serviceCollection.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+        {
+            options.RequireHttpsMetadata = false;
+            options.SaveToken = true;
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidAudience = configuration["Jwt:Audience"],
+                ValidIssuer = configuration["Jwt:Issuer"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]!))
+            };
+        });
+        return serviceCollection;
+    }
+}
--- Login/Program.cs
+++ Login/Program.cs
@@ -1,5 +1,8 @@
+using Login.Application;
 using Login.Context;
 using Login.Extensions;
+using Login.IRepositories;
+using Login.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,8 +11,16 @@ var configuration = builder.Configuration;
 
 builder.Services.AddDbContext<LoginContext>(options => options.UseSqlite(configuration.GetConnectionString("sqlite")));
 
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+builder.Services.AddScoped<CreateUserUseCase>();
+builder.Services.AddScoped<GetUsersUseCase>();
+builder.Services.AddAuth(configuration);
+
 builder.Services.AddSwagger();
 builder.Services.AddControllers();
+builder.Services.AddAuthentication();
+
 
 var app = builder.Build();
 
@@ -21,5 +32,7 @@ using (var scope = app.Services.CreateScope())
 
 app.UseSwagger();
 app.UseSwaggerUI(options => { options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"); });
+
+app.MapControllers();
 
 app.Run();
