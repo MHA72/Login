764d76872a059e35b699e93e3ace9a9e7a85ceba
MohammadHossein <hossinarab727@gmail.com>
2023-11-02 13:07:24 +0330

    Add Config

---
+++ Login/Configurations/User/UserConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Login.Configurations.User;
+
+public sealed class UserConfiguration : IEntityTypeConfiguration<Models.User.User>
+{
+    public void Configure(EntityTypeBuilder<Models.User.User> builder)
+    {
+        builder.HasKey(user => user.Id);
+        builder.HasQueryFilter(user => !user.IsDeleted);
+        builder.HasIndex(user => user.Username).IsUnique();
+
+        builder.Property(user => user.Username).IsRequired().IsUnicode(false);
+        builder.Property(user => user.Email).IsRequired().IsUnicode(false);
+        builder.Property(user => user.PasswordHash).IsRequired().IsUnicode(false);
+
+        builder.Property(user => user.UpdateTime)
+            .HasConversion(time => time.ToUniversalTime(), time => time.ToUniversalTime());
+        builder.Property(user => user.InsertTime)
+            .HasConversion(time => time.ToUniversalTime(), time => time.ToUniversalTime());
+
+    }
+}
---
+++ Login/Configurations/User/UserLoginConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Login.Models.User;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Login.Configurations.User;
+
+public sealed class UserLoginConfiguration : IEntityTypeConfiguration<UserLogin>
+{
+    public void Configure(EntityTypeBuilder<UserLogin> builder)
+    {
+        builder.HasKey(userLogin => userLogin.Id);
+
+        builder.Property(userLogin => userLogin.UpdateTime)
+            .HasConversion(time => time.ToUniversalTime(), time => time.ToUniversalTime());
+        builder.Property(userLogin => userLogin.InsertTime)
+            .HasConversion(time => time.ToUniversalTime(), time => time.ToUniversalTime());
+
+        builder.HasOne(userLogin => userLogin.User)
+            .WithMany(user => user.UserLogins)
+            .HasForeignKey(userLogin => userLogin.UserId)
+            .IsRequired();
+
+        builder.HasIndex(userLogin => new { userLogin.UserId, userLogin.IsDeleted }).IsUnique();
+    }
+}
