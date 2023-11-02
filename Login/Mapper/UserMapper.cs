e71e94506152a25810917a4a84e33cad7cc6564e
MohammadHossein <hossinarab727@gmail.com>
2023-11-02 13:55:51 +0330

    Add Mapper

---
+++ Login/Mapper/UserMapper.cs
@@ -0,0 +1,9 @@
+﻿using Login.Dtos.Info;
+using Login.Models.User;
+
+namespace Login.Mapper;
+
+public static class UserMapper
+{
+    public static UserInfo ToUserInfo(this User user) => new(user.Id, user.Username, user.Email);
+}
