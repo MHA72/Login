a02c19dfd3aba06a7e9ebea6aee5c05eced90211
MohammadHossein <hossinarab727@gmail.com>
2023-11-02 13:56:35 +0330

    Add User Interface

---
+++ Login/IRepositories/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Login.Dtos.Info;
+using Login.Dtos.Requests;
+using Login.Models.User;
+
+namespace Login.IRepositories;
+
+public interface IUserRepository
+{
+    Task<User?> GetUserByUsername(string username, CancellationToken cancellationToken = default);
+    Task<User?> GetUserById(Guid userId, CancellationToken cancellationToken = default);
+    Task AddLogin(Guid userId, CancellationToken cancellationToken = default);
+    Task DeleteAllLogins(Guid userId, CancellationToken cancellationToken = default);
+    Task<(List<UserInfo>, int)> GetUsers(QueryParameter parameter, CancellationToken cancellationToken = default);
+    void AddUserSync(User user);
+    Task<UserInfo> AddUser(User user, CancellationToken cancellationToken = default);
+}
